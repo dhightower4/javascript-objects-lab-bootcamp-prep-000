1cb73fbd13428a50f9bddc0f4423bd55b4b38675
Donte Hightower <dontehightower12@gmail.com>
2017-09-01 18:50:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = { key : "value"}
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj.key = "value"
+  return obj
+}
